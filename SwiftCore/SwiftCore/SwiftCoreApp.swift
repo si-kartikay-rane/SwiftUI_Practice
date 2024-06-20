afd1790081c99601b032916954d89e018526a046
Kartikay Rane <kartikay.rane@sportzinteractive.net>
2024-06-20 18:09:06 +0530

    New file for Swift

---
+++ SwiftCore/SwiftCore/SwiftCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoreApp.swift
+//  SwiftCore
+//
+//  Created by Kartikay Rane on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
